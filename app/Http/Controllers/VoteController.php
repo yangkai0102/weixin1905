3d4342aa8d3b3f4307467814680f7087705cd1ca
yangkai <yangkai@localhost.localdomain>
2019-12-19 01:09:08 +0000

    n

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -29,9 +29,9 @@ class VoteController extends Controller
 
         $total=Redis::zCard($key);   //获取总数
         echo '投票总人数：'.$total;echo '</br>';
-        $menmber=Redis::zRange($key,0,-1,true);
+        $member=Redis::zRange($key,0,-1,true);
 //        echo '<pre>';print_r($menmber);echo '</pre>';
-        foreach($menmber as $k=>$v){
+        foreach($member as $k=>$v){
             echo "用户：".$k .' 投票时间：'. date('Y-m-d H:i:s',$v);echo '</br>';
         }
     }
