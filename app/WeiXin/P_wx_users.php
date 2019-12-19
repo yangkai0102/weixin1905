fdb40fbe2d1eea2d09768f63843a3a617cce148b
yangkai <yangkai@localhost.localdomain>
2019-12-19 06:23:04 +0000

    gaicuo

--- app/WeiXin/P_wx_users.php
+++ app/WeiXin/P_wx_users.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class P_wx_users extends Model
 {
-    //
+    protected $table='p_wx_users';
+
+    protected $primaryKey='uid';
+
+    public $timestamps = false;
+
+    protected $guarded = [];
 }
