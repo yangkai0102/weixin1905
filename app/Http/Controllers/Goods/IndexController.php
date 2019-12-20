5ad3e267dded18949445c46b1ccef6de34e78fec
yangkai <yangkai@localhost.localdomain>
2019-12-20 10:19:51 +0000

    houtai shangpinguanli

--- app/Http/Controllers/Goods/IndexController.php
+++ app/Http/Controllers/Goods/IndexController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers\Goods;
 
 use App\Http\Controllers\Controller;
+use App\WeiXin\GoodsModel;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     //商品详情页
-    public function detail(){
-        return view('goods.detail');
+    public function detail(Request $request){
+        $goods_id=$request->input('id');
+//        echo $goods_id;die;
+        $goods=GoodsModel::find($goods_id);
+//        print_r($goods);
+        $data=[
+            'goods'=>$goods
+        ];
+        return view('goods.detail',$data);
     }
 }
--- resources/views/goods/detail.blade.php
+++ resources/views/goods/detail.blade.php
@@ -366,9 +366,9 @@
 <div class="pages section">
     <div class="container">
         <div class="shop-single">
-            <img src="/img/shop-single.png" alt="">
-            <h5>Fashion Men's</h5>
-            <div class="price">$20 <span>$28</span></div>
+            <img src="/storage/{{$goods->img}}" alt="">
+            <h5>{{$goods->goods_name}}</h5>
+            <div class="price">${{$goods->price}} <span>$28</span></div>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam eaque in non delectus, error iste veniam commodi mollitia, officia possimus, repellendus maiores doloribus provident. Itaque, ab perferendis nemo tempore! Accusamus</p>
             <button type="button" class="btn button-default">ADD TO CART</button>
         </div>
