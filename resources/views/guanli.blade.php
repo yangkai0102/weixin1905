2ccf6654386b46e425ea8680b81530df4afa369c
yangkai <yangkai@localhost.localdomain>
2019-12-26 03:31:52 +0000

    gaicuo

--- resources/views/guanli.blade.php
+++ resources/views/guanli.blade.php
@@ -8,12 +8,13 @@
     <title>Document</title>
 </head>
 <body>
-<form action="{{url('http://1905yangkai.comcto.com/wx/guanlido/'.$code)}}" method="post">
+<form action="{{url('http://1905yangkai.comcto.com/wx/guanlido')}}" method="post">
     @csrf
     第一节课: <input type="text" name="a1"><br>
     第二节课: <input type="text" name="a2"><br>
     第三节课: <input type="text" name="a3"><br>
     第四节课: <input type="text" name="a4"><br>
+    <input type="hidden" name="code" value="{{$code}}">
     <input type="submit" value="提交">
 </form>
 </body>
